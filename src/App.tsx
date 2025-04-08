f87bb700f2a3a219bb78b482fa84228421fb7f5b
Guilherme Torres <guiatorres8@gmail.com>
2025-04-08 12:51:18 -0300

    start header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import './global.css';
-import { Post } from './Post';
+import styles from './App.module.css';
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 
 function App() {
 	return (
 		<>
 			<Header />
-			<h1>Hello World</h1>
 
-			<Post author="Guilherme" content="lorem" />
+			<div className={styles.wrapper}>
+				<Sidebar />
+				<main>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam,
+					commodi aliquam dolor facere unde sit fugit perspiciatis fugiat
+					molestiae magni assumenda. Reprehenderit eum architecto eaque culpa
+					ratione harum consequatur odio?
+				</main>
+			</div>
 		</>
 	);
 }
