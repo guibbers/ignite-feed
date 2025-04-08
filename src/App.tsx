46894c2ad7f9feed1f8a261989374978185c3cae
Guilherme Torres <guiatorres8@gmail.com>
2025-04-08 12:12:49 -0300

    add globals css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import './global.css';
+import { Post } from './Post';
+import { Header } from './components/Header';
+
 function App() {
 	return (
 		<>
+			<Header />
 			<h1>Hello World</h1>
+
+			<Post author="Guilherme" content="lorem" />
 		</>
 	);
 }
