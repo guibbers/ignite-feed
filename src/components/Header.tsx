85d56f205952612aa7d5b0bb2615fbf52c3c64dd
Guilherme Torres <guiatorres8@gmail.com>
2025-06-01 01:25:28 -0300

    add header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,11 @@
+import igniteLogo from '../assets/Ignite simbol.svg';
 import styles from './Header.module.css';
 
 export function Header() {
 	return (
 		<header className={styles.header}>
-			<strong>Ignite Feed</strong>
+			<img src={igniteLogo} alt="logotipo do ignite" />
+			<h2>Ignite Feed</h2>
 		</header>
 	);
 }
